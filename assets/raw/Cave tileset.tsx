4ad6f9e9b87a9ff5a1ebc7993ecaaab7ed45abce
Christine Dodrill <me@christine.website>
2020-12-27 16:57:56 -0500

    more content

---
+++ assets/raw/Cave tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Cave tileset" tilewidth="8" tileheight="8" tilecount="264" columns="22">
+ <image source="Cave tileset.png" width="176" height="96"/>
+</tileset>
---
+++ assets/raw/DungeonTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="Tileset gameboy2" tilewidth="8" tileheight="8" tilecount="182" columns="14">
+ <image source="DungeonTileset.png" width="112" height="104"/>
+</tileset>
