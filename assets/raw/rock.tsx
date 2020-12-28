87093f66a2d5fda53577005d51ab191b79dc7d04
Christine Dodrill <me@christine.website>
2020-12-27 22:24:34 -0500

    yes

---
+++ assets/raw/rock.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="rock" tilewidth="8" tileheight="8" tilecount="4" columns="2">
+ <image source="../sprites/rock.png" width="16" height="16"/>
+</tileset>
